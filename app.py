bd164538806b9190bb1bf9ac726ff189329f0444
JasPradhan <jaspradhantheman@gmail.com>
2022-08-05 21:46:05 +0530

    first commit

--- app.py
+++ app.py
@@ -12,19 +12,45 @@ app = Flask(__name__)
 def upload_form():
     return render_template('upload.html')
 
+i='0'
 
 @app.route('/', methods=['POST'])
+
+
 def upload_image():
     #write the code from here
+    global i
+
+    x=i+'0'
+
+    print("BEFORE 1: "+str(x))
+
     image_file=request.files['file']
+
     degree=int(request.form['text'])
+
     filename=secure_filename(image_file.filename)
+
     image_file.save(os.path.join('static/',filename))
+
     image=Image.open(image_file)
+
     image_rotation_degree=image.rotate(degree)
-    image_rotation_degree.save(os.path.join('static/','rotated_image.jpg'))
-    img_rotate='rotated_image.jpg'
+
+    print("BEFORE 2: "+str(x))
+
+    image_rotation_degree.save(os.path.join('static/','rotated_image_'+str(x)+'.jpg'))
+
+    img_rotate='rotated_image_'+str(x)+'.jpg'
+    
     return render_template('upload.html',filename=img_rotate)
+
+    i='10'
+
+    return i
+
+    print("after 1: "+str(x))   
+
 
 @app.route('/display/<filename>')
 def display_image(filename):
